77503c0e09c0a520ffdc3b4f936c579148acd915
Nicolas Kuttler <office@nicolaskuttler.com>
2012-04-03 14:14:02 +0200

    Clean up the code a bit
    Probably shouldn't have copied so much from the PHP implementation..

--- piwik_tracking/piwiktracker.py
+++ piwik_tracking/piwiktracker.py
@@ -24,7 +24,8 @@ class PiwikTracker:
         self.user_agent = self.request.META.get('HTTP_USER_AGENT', '')
         self.referer = self.request.META.get('HTTP_REFERER', '')
         self.ip = self.request.META.get('REMOTE_ADDR')
-        self.accept_language = self.request.META.get('HTTP_ACCEPT_LANGUAGE', '')
+        self.accept_language = self.request.META.get('HTTP_ACCEPT_LANGUAGE',
+            '')
 
     def set_local_time(self, datetime):
         self.local_hour = datetime.hour
@@ -52,7 +53,7 @@ class PiwikTracker:
         return self.request.META.get('QUERY_STRING', '')
 
     def get_current_url(self):
-        url  = self.get_current_scheme() + '://'
+        url = self.get_current_scheme() + '://'
         url += self.get_current_host()
         url += self.get_current_script_name()
         url += self.get_current_query_string()
@@ -62,19 +63,20 @@ class PiwikTracker:
         return datetime.datetime.now()
 
     def get_query_vars(self, document_title=False):
-        url = "?idsite=%d&rec=1&apiv=%s&r=%s&url=%s&urlref=%s&cip=%s&token_auth=%s" % (
-            self.id_site,
-            self.VERSION,
-            random.randint(0, 99999),
-            urllib.quote_plus(self.page_url),
-            urllib.quote_plus(self.referer),
+        parameters = {
+            'idsite': self.id_site,
+            'rec': 1,
+            'apiv': self.VERSION,
+            'r': random.randint(0, 99999),
+            'url': self.page_url,
+            'urlref': self.referer,
             # Forcing IP requires the auth token
-            self.ip,
-            self.token_auth,
-        )
+            'cip': self.ip,
+            'token_auth': self.token_auth,
+        }
         if document_title:
-            url += '&action_name=%s' % urllib.quote_plus(document_title)
-        return url
+            parameters['action_name'] = urllib.quote_plus(document_title)
+        return urllib.urlencode(parameters)
 
     def send_request(self, query_vars):
         "Send the request to piwik"
@@ -84,15 +86,17 @@ class PiwikTracker:
         }
         parsed = urlparse.urlparse(self.api_url)
         connection = httplib.HTTPConnection(parsed.hostname)
-        url = parsed.path + query_vars
+        url = "%s?%s" % (parsed.path, query_vars)
         connection.request('GET', url, '', headers)
         response = connection.getresponse()
         return response.read()
 
     def do_track_page_view(self, document_title):
         query_vars = self.get_query_vars(document_title)
-        return self.send_request(query_vars);
+        return self.send_request(query_vars)
 
-def piwik_get_url_track_page_view(id_site, api_url, request, token_auth, document_title=False):
+
+def piwik_get_url_track_page_view(id_site, api_url, request, token_auth,
+        document_title=False):
     tracker = PiwikTracker(id_site, api_url, request, token_auth)
     return tracker.do_track_page_view(document_title)
