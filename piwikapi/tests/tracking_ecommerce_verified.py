027e6df73249085271e2899cb60ab9c4628a227d
Nicolas Kuttler <office@nicolaskuttler.com>
2012-04-11 16:21:55 +0200

    Add first tests for ecommerce tracking

--- piwikapi/tests/tracking_ecommerce_verified.py
+++ piwikapi/tests/tracking_ecommerce_verified.py
@@ -2,7 +2,7 @@ import urllib
 from random import randint
 
 from piwikapi.tracking import PiwikTrackerEcommerce
-from tracking import TrackerBaseTestCase
+from tracking_verified import TrackerVerifyBaseTestCase
 
 
 class TrackerEcommerceBaseTestCase(TrackerVerifyBaseTestCase):
@@ -35,59 +35,90 @@ class TrackerEcommerceBaseTestCase(TrackerVerifyBaseTestCase):
         },
     }
 
+
+class TrackerEcommerceVerifyTestCase(TrackerEcommerceBaseTestCase):
     def setUp(self):
-        super(TrackerEcommerceBaseTestCase, self).setUp()
+        super(TrackerEcommerceVerifyTestCase, self).setUp()
+        # Set different IP for each test run
+        # TODO also randomize referers etc...
         self.pte = PiwikTrackerEcommerce(self.settings.PIWIK_SITE_ID,
                                          self.request)
         self.pte.set_api_url(self.settings.PIWIK_TRACKING_API_URL)
-
-
-class TrackerEcommerceNoverifyTestCase(TrackerEcommerceBaseTestCase):
-    """
-    Ecommerce unit tests
-    """
-    def setUp(self):
-        """
-        Ecommerce tracking data doesn't always show up in the Live API
-        """
-
-    def test_browse_cart_update_order(self):
-        """
-        This is a test shopping run. The visitor will browse from category to
-        product page and buy a random amount of products. These values should
-        probably be hardcoded so that we can verify them.. or maybe we can use
-        the analytics API once we have written it..
-        """
-        # Set different IP for each test run
-        # TODO also randomize referers etc...
         self.pte.set_ip(self.get_random_ip())
         self.pte.set_token_auth(self.settings.PIWIK_TOKEN_AUTH)
+        self.pte.set_custom_variable(
+            5,
+            'testsegment',
+            self.segment,
+        )
+        self.pte._set_host("ecommerce.example.com")
+        self.pte._set_query_string('')
 
-        grand_total = 0.0
+        self.a.set_method('Live.getLastVisitsDetails')
+        self.a.set_parameter('lastMinutes', 1)
+        self.a.set_segment("customVariableName5==testsegment;"
+            "customVariableValue5==%s" % self.segment)
+
+    def test_ecommerce_view(self):
+        # View a product
+        product = self.products['book']
+        script = "/view/%s/" % urllib.quote(product['name'])
+        self.pte._set_script(script)
+        self.pte.set_ecommerce_view(
+            product['sku'],
+            product['name'],
+            product['category'],
+            product['price'],
+        )
+        title = self.get_title('Product view %s' % product['name'])
+        r = self.pte.do_track_page_view(title)
+        self.assertEqual(
+            title,
+            self.get_av('pageTitle'),
+            'Title: "%s", not "%s"' % (self.get_av('pageTitle'), title)
+        )
+        self.assertEqual(
+            product['name'],
+            self.get_cv(4),
+            "Product name %s, not %s" % (self.get_cv(4), product['name'])
+        )
+
+    #def test_add_ecommerce_item(self):
+    #    """
+    #    TODO can't test this directly
+    #    """
+    #    grand_total = 0
+    #    for key, product in self.products.iteritems():
+    #        # Put product in the cart
+    #        quantity = randint(1, product['quantity'])
+    #        self.pte.add_ecommerce_item(
+    #            product['sku'],
+    #            product['name'],
+    #            product['category'],
+    #            product['price'],
+    #            quantity,
+    #        )
+    #        grand_total += product['price'] * quantity
+
+    #    title = self.get_title('Add ecommerce items')
+    #    r = self.pte.do_track_page_view(title)
+    #    import json
+    #    data = json.loads(self.a.send_request())
+    #    self.debug(data)
+    #    self.assertEqual(
+    #        title,
+    #        self.get_av('pageTitle'),
+    #        'Title: "%s", not "%s"' % (self.get_av('pageTitle'), title)
+    #    )
+
+    def test_track_ecommerce_order(self):
+        """
+        TODO We could test that each product was added, not only the sums
+        """
+        grand_total = 0
+        quantity_total = 0
         for key, product in self.products.iteritems():
-            if randint(0, 2) == 0:
-                continue
-
-            # View the category page
-            category = product['category'][1]
-            self.pte._set_script("/category/%s/" % urllib.quote(category))
-            r = self.pte.do_track_page_view(self.get_title('Category %s' %
-                                            category))
-
-            # View the product
-            self.pte._set_host("ecommerce.example.com")
-            self.pte._set_query_string('')
-            self.pte._set_script("/view/%s/" % urllib.quote(product['name']))
-            self.pte.set_ecommerce_view(
-                product['sku'],
-                product['name'],
-                product['category'],
-                product['price'],
-            )
-            r = self.pte.do_track_page_view(self.get_title('view %s'
-                                            % product['name']))
-
-            # Put it in the cart
+            # Put product in the cart
             quantity = randint(1, product['quantity'])
             self.pte.add_ecommerce_item(
                 product['sku'],
@@ -97,12 +128,24 @@ class TrackerEcommerceNoverifyTestCase(TrackerEcommerceBaseTestCase):
                 quantity,
             )
             grand_total += product['price'] * quantity
-
+            quantity_total += quantity
         # Order the products
-        #r = self.pte.do_track_ecommerce_cart_update(grand_total)
-        self.pte._set_script("/cart/checkout/")
-        #print repr(grand_total)
+        script = "/cart/checkout/"
+        self.pte._set_script(script)
         r = self.pte.do_track_ecommerce_order(
             randint(0, 99999), # TODO random failure
             grand_total,
+        )
+        grand_total_string = "%.2f" % grand_total
+        revenue = self.get_av('revenue')
+        self.assertEqual(
+            grand_total_string,
+            revenue,
+            "Grand total %s, not %s" % (revenue, grand_total_string),
+        )
+        items = self.get_av('items')
+        self.assertEqual(
+            str(quantity_total),
+            items,
+            "Quantity %s, not %s" % (items, quantity_total),
         )
