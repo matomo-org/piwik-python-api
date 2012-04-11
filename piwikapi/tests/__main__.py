e5e9f836672db1feeea4e639797c45f23db0ea4d
Nicolas Kuttler <office@nicolaskuttler.com>
2012-04-11 14:44:32 +0200

    Revert "Bugfix"
    This reverts commit 79fb6f7d7a2f09a5bc001ec9d90461bf9ffa0c4d.

--- piwikapi/tests/__main__.py
+++ piwikapi/tests/__main__.py
@@ -2,9 +2,9 @@ import unittest
 
 from analytics import AnalyticsTestCase
 from analytics import AnalyticsLiveTestCase
+from ecommerce import TrackerEcommerceNoverifyTestCase
 from tracking import TrackerClassTestCase
 from tracking import TrackerTestCase
-#from tracking_ecommerce_verified import TrackerEcommerceVerifyTestCase
 from tracking_verified import TrackerVerifyTestCase
 
 if __name__ == '__main__':
