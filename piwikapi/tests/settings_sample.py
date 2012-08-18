34318b87bb2123fddae515662abfc69af3c2c135
Nicolas Kuttler <git@kuttler.eu>
2012-08-18 22:57:22 +0200

    Documentation updates

--- piwikapi/tests/settings_sample.py
+++ piwikapi/tests/settings_sample.py
@@ -5,13 +5,14 @@ class Settings:
     #: The ID of the site you want to send the test tracking requests to
     PIWIK_SITE_ID = 1
 
-    #: This must contain the URL to your Piwik install's /piwik.php
+    #: This must contain the URL to your Piwik install's /piwik.php, something
+    #: like 'http://example.com/piwik.php'
     PIWIK_TRACKING_API_URL = '<Your Piwik tracker API URL>'
 
     #: This must contain the URL to your Piwik install's root
     PIWIK_ANALYTICS_API_URL = '<Your Piwik API URL>'
 
-    #: The auth token of an admin user for above site
+    #: The auth token of an admin user for the site defined in PIWIK_SITE_ID
     PIWIK_TOKEN_AUTH = '<Your Piwik auth token>'
 
     #: The ID of the goal that will be used for unit tests.
