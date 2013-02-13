1b52c4dc8da3a4404e69314a882144ad1c9b749e
Nicolas Kuttler <git@kuttler.eu>
2013-02-13 21:44:07 +0100

    Doc updates

--- piwikapi/tests/request.py
+++ piwikapi/tests/request.py
@@ -2,7 +2,8 @@ class FakeRequest:
     """
     A replacement for Django's Request object. This is only used for unit
     tests at the moment. If you're not using Django and need to create such
-    a class have a look at the source for the unit tests in tests.py.
+    a class have a look at the source for the tests fake request class in
+    in tests/request.py
     """
     #: Boolean, if the connection is secured or not
     secure = False
