fb68a075c2161512abd826d29b5e7527c0e46ce0
Nicolas Kuttler <git@kuttler.eu>
2013-02-17 20:51:09 +0100

    Fix version string

--- doc/conf.py
+++ doc/conf.py
@@ -56,7 +56,7 @@ copyright = u'2012-2013, Nicolas Kuttler'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.1'
+version = '0.2.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
