f170f0d653fcc10a8aeda49e24843b6fda6a6f3f
Nicolas Kuttler <git@kuttler.eu>
2015-04-09 20:53:25 +0200

    See if this fixes the rtd build

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ sys.path.insert(0, os.path.abspath('..'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
-    'sphinxcontrib.fulltoc',
+    #'sphinxcontrib.fulltoc',
 ]
 
 intersphinx_mapping = {
